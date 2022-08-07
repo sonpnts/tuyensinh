866359073f7f38b34420eaa0a82182ef612ff93c
sonpnts <108865028+sonpnts@users.noreply.github.com>
2022-08-07 16:05:15 +0700

    7-8

--- assets/js/java.js
+++ assets/js/java.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
     $(".play-yt").click(function () {
         $(".play-yt").removeClass("open");
     });
+    $(".play-yt").click(function () {
+        $("#video-yt").remove();
+    });
+    $("#close").click(function(){
+        $("#video-yt").remove();
+    });
+    $(".animate__animated").hover(function(){
+        $(this).addClass("animate__bounceIn");
+    }, function(){
+        $(this).removeClass("animate__bounceIn");
+    })
 });
