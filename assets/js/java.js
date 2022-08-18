e7c25b0bf3bff397ab833caeafcf0c596029211e
sonpnts <108865028+sonpnts@users.noreply.github.com>
2022-08-18 12:33:29 +0700

    change

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
     $(".img-yt span").click(function () {
+       
         $(".play-yt").addClass("open");
+        $("#video-yt").attr("src","https://www.youtube.com/embed/6F6RGmVvfdM?rel=0");
     });
     $("#close").click(function(){
         $(".play-yt").removeClass("open");
+        $("#video-yt").attr("src"," ");
     });
     $(".play-yt").click(function () {
         $(".play-yt").removeClass("open");
-    });
-    $(".play-yt").click(function () {
-        $("#video-yt").remove();
-    });
-    $("#close").click(function(){
-        $("#video-yt").remove();
+        $("#video-yt").attr("src"," ");
     });
     $(".animate__animated").hover(function(){
-        $(this).addClass("animate__bounceIn");
+        $(this).addClass("animate__bounce");
     }, function(){
-        $(this).removeClass("animate__bounceIn");
+        $(this).removeClass("animate__bounce");
+    })
+    $(".regis form").on("click","input#DK",function(){
+        
+    })
+    $(".backtop").click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },500);
+    });
+    $(window).scroll(function(){
+        if($(this).scrollTop()){
+            $(".menu-scroll").fadeIn();
+            $("#header").fadeOut();
+        }else{
+            $(".menu-scroll").fadeOut();
+            $("#header").fadeIn();
+        }
     })
 });
